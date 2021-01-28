5b7adc0b8e59633105fd86cb6662671932e4f608
Firas Altayeb <tayebfiras@gmail.com>
2021-01-28 14:47:24 +0900

    string manipulation practice

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -1,1 +1,5 @@
 print('Hello, world')
+print(1 + 2)
+print(7 * 8)
+print()
+print("The end", 'or is it', 'keep watching', 3)
---
+++ HelloWorld/strings.py
@@ -0,0 +1,16 @@
+print("today's a good day" + 'but tomorrow might')
+greetings = "hello"
+# name = input('please enter your name \n')
+print(greetings + " " + 'James')
+
+age = 24
+print(age)
+print(type(greetings))
+print(type(age))
+
+title = 'mr'
+
+# print(title + ' james is ' + age)
+
+age = "2 years"
+print(type(age))
