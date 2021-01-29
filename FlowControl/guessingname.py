e077d4acf3226e820ff6d3a76e8c9692a9e389e3
Firas Altayeb <tayebfiras@gmail.com>
2021-01-29 19:43:58 +0900

    playing with conditionals p2

--- FlowControl/guessingname.py
+++ FlowControl/guessingname.py
@@ -5,8 +5,17 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Guessed right")
+    else:
+        print("wrong")
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Guessed right")
+        print("wrong")
 else:
     print("You got it first time")
 
