2dd4d131656116c10aae1c7daa08da6707d9ff7c
Firas Altayeb <tayebfiras@gmail.com>
2021-02-12 20:58:24 +0900

    playing with pangrams

--- Sequences/number_list.py
+++ Sequences/number_list.py
@@ -1,15 +1,33 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7]
 
-print(min(even))
-print(max(odd))
-print(min(even))
-print(max(odd))
+numbers = even + odd
+print(numbers)
 
-print()
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
 
-print(len(even))
-print(len(odd))
+digits = list("432985617")
+print(digits)
 
-print()
-print("mississippi".count("s"))
+# more_numbers = list(numbers)
+# more_numbers = numbers[:]
+more_numbers = numbers.copy()
+print(more_numbers)
+print(numbers is more_numbers)
+print(numbers == more_numbers)
+
+# print(min(even))
+# print(max(odd))
+# print(min(even))
+# print(max(odd))
+#
+# print()
+#
+# print(len(even))
+# print(len(odd))
+#
+# print()
+# print("mississippi".count("s"))
