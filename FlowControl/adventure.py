df01e1e556d10779388384259f79e1c8aae79a77
Firas Altayeb <tayebfiras@gmail.com>
2021-02-15 16:06:42 +0900

    code for snake game using pygames

--- FlowControl/adventure.py
+++ FlowControl/adventure.py
@@ -2,7 +2,7 @@ available_exits = ["north", "south", "east", "west"]
 
 chosen_exit = ""
 while chosen_exit not in available_exits:
-    chosen_exit = input("Please choose a direction: ")
+    chosen_exit = input("Please choose a direction: ").casefold()   
     if chosen_exit.casefold() == "quit":
         print("Game over")
         break
