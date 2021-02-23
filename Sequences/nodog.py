c06c074281a04cb5655223c542bd9d91904bcd7e
Firas Altayeb <tayebfiras@gmail.com>
2021-02-23 23:49:04 +0900

    practice with inner lists

---
+++ Sequences/nodog.py
@@ -0,0 +1,24 @@
+menu = [
+    ["cheese", "dog"],
+    ["cheese", "dog", "bread"],
+    ["cheese", "bread"],
+    ["cheese", "dog", "pickles"],
+    ["cheese", "pickles"],
+    ["pickles", "dog"],
+    ["pickles", "bread",],
+    ["cheese", "dog", "dog", "dog", "bread", "pickles"],
+]
+
+for meal in menu:
+    for item in meal:
+        if item != "dog":
+            print(item)          
+    print()
+
+
+# for meal in menu:
+#     for index in range(len(meal) - 1, -1, -1):
+#         if meal[index] == "dog":
+#             del meal[index] 
+            
+#     print(meal)
--- Sequences/number_list.py
+++ Sequences/number_list.py
@@ -2,9 +2,14 @@ empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7]
 
-numbers = even + odd
+numbers = [even , odd]
 print(numbers)
 
+for number_list in numbers:
+    print(number_list)
+
+
+"""
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 print(numbers)
@@ -31,3 +36,4 @@ print(numbers == more_numbers)
 #
 # print()
 # print("mississippi".count("s"))
+"""
