64e296703a0461c839683dc0b5d58d84c3845861
Firas Altayeb <tayebfiras@gmail.com>
2021-02-25 23:35:17 +0900

    working with split and join

---
+++ Sequences/splitting_things.py
@@ -0,0 +1,21 @@
+panagram = """The quick brown
+ for jumps\tover
+ the lazy dog"""
+
+words = panagram.split()
+print(words)
+
+number = "9,443,112,443,342,112"
+print(number.split(","))
+
+generated_list = ['9', ' ',
+                  '2', '2', '3', ' ',
+                  '6', '5', '6', ' ',
+                  '7', '7', '5', ' ',
+                  '6', '9', '9']
+
+values = "".join(generated_list)
+print(values)
+
+values_list = values.split()
+print(values_list)
