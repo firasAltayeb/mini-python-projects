84c8e8c3fb3c9e06dedb490f76e7fbf98417c845
Firas Altayeb <tayebfiras@gmail.com>
2021-03-01 23:52:04 +0900

    more tuple unpacking

--- Sequences/tuples_intro.py
+++ Sequences/tuples_intro.py
@@ -11,3 +11,17 @@ print(letters2)
 
 letters2[0] = "a2"
 print(letters2)
+
+metallica = "Ride the lighting", "Metallica", 1984
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
+
+table = ("Coffe table", 200, 100, 75, 34.5)
+print(table[1] * table[2])
+
+name, length, width, height, price = table
+print(length * width)
+
+
