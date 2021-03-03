d001049c562bd6feb994b6673f6d5b8911126de9
Firas Altayeb <tayebfiras@gmail.com>
2021-03-03 17:34:43 +0900

    playing with jukebox code

--- Sequences/jukebox.py
+++ Sequences/jukebox.py
@@ -1,6 +1,7 @@
 from nested_data import albums
 
 SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
 while True:
     print("Please choose your album (invalid choice exits):")
     for index,  (title, artist, year, songs) in enumerate(albums):
@@ -11,6 +12,17 @@ while True:
         songs_list = albums[choice - 1][SONGS_LIST_INDEX]
     else:
         break
+
+    print("Please choose your songs:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
+
+    song_choice = int(input())
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+        print("Playing {}".format(title))
+
+    print("=" * 40)
 
 
 
