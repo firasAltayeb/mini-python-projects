bdc8f4e9567f3ff19396de456204115479b4eb57
Firas Altayeb <tayebfiras@gmail.com>
2021-03-07 01:55:35 +0900

    playing with default parameters

---
+++ FunctionIntro/banner.py
@@ -0,0 +1,21 @@
+def banner_text(text, width=80):
+    screen_width = width
+    if len(text) > screen_width - 4:
+        raise ValueError("String {0} is larger then specified "
+                         "width {1}".format(text, screen_width))
+
+    if text == "*":
+        print("*" * screen_width)
+    else:
+        centered_text = text.center(screen_width - 4)
+        output_string = "**{0}**".format(centered_text)
+        print(output_string)
+
+
+banner_text("*", 50)
+banner_text("Always look on the bright side")
+banner_text(" ", 50)
+banner_text("When you're feeling in the dumps")
+banner_text("*", 10)
+
+
