0c2e281a706a6364f64cab53cf2ce288a48035ce
Firas Altayeb <tayebfiras@gmail.com>
2021-03-09 23:34:42 +0900

    added python docs for the first time

--- FunctionIntro/guessinggame.py
+++ FunctionIntro/guessinggame.py
@@ -8,6 +8,16 @@ print("Please guess number between 1 and {}: ".format(highest))
 
 
 def get_integer(prompt):
+    """
+    Get an integer from the standard Input (stdin)
+
+    The function will continue looping, and prompting
+    the user, until a valid int is entered.
+
+    :param prompt: The String that the user will see, when
+        they're prompted to enter the value.
+    :return: The integer that the user enters.
+    """
     while True:
         temp = input(prompt)
         if temp.isnumeric():
