434862bbccd7df2c51c3a2f4369f948573287d21
Firas Altayeb <tayebfiras@gmail.com>
2021-03-17 23:07:05 +0900

    tsunami variables now make more since

--- Minecraft/tsunami.py
+++ Minecraft/tsunami.py
@@ -4,16 +4,16 @@ from time import sleep
 mc = Minecraft.create("mc2.tokyocodingclub.com")
 
 water = 8
-direction = 1
+distance = 1
 
 playerId = mc.getPlayerEntityId("TCCMinecraft001")
 x, y, z = mc.entity.getPos(playerId)
 
 while True:
-    mc.setBlocks(x+direction, y, z, x+direction, y, z+10, water)
-    mc.setBlocks(x+direction, y, z, x+direction, y, z-10, water)
-    direction += 1
-    sleep(0.1)            
+    mc.setBlocks(x + distance, y, z, x + distance, y, z + 10, water)
+    mc.setBlocks(x + distance, y, z, x + distance, y, z - 10, water)
+    distance += 1
+    sleep(1.0)
 
 
 
