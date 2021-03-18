6e703585ad885f3e153e2af286574440ad340b00
Firas Altayeb <tayebfiras@gmail.com>
2021-03-18 20:50:41 +0900

    testing different parameter types

--- FunctionIntro/colour_print.py
+++ FunctionIntro/colour_print.py
@@ -16,21 +16,25 @@ UNDERLINE = '\u001b[4m'
 REVERSE = '\u001b[7m'
 
 
-def colour_print(text: str, effect: str) -> None:
+def colour_print(text: str, *effects: str) -> None:
     """
     Print text using the ANSI sequences to change color, etc
     :param text: The text to print
-    :param effect: The effect we want, on of the constants
+    :param effects: The effect we want, on of the constants
     """
-    output_string = "{0}{1}{2}".format(effect, text, RESET)
+    effects_string = "".join(effects)
+    output_string = "{0}{1}{2}".format(effects_string, text, RESET)
     print(output_string)
 
 
 colorama.init()
 colour_print("Hello, Red", RED)
+colour_print("Hello, Red in bold", RED, BOLD)
 # test that the colour was reset
 print("This should be in the default terminal colour")
 colour_print("Hello, Blue", BLUE)
+colour_print("Hello, Blue reversed", BLUE, REVERSE)
+colour_print("Hello, Blue reversed and underlined", BLUE, REVERSE, UNDERLINE)
 colour_print("Hello, Yellow", YELLOW)
 colour_print("Hello, Bold", BOLD)
 colour_print("Hello, Underline", UNDERLINE)
