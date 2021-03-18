d51d6e31c5a32711504c21a2423afcec3e6385d1
Firas Altayeb <tayebfiras@gmail.com>
2021-03-18 19:48:39 +0900

    completed the fizzbuzz game and tested *args

--- FunctionIntro/fizzbuzz_challenge.py
+++ FunctionIntro/fizzbuzz_challenge.py
@@ -18,5 +18,18 @@ def fizz_buzz(number: int) -> str:
         return str(number)
   
   
-for i in range(1, 101):
-    print(fizz_buzz(i))
+input("Play Fizz Buzz. Press ENTER to start")
+print()
+
+next_number = 0
+while next_number < 99:
+    next_number += 1
+    print(fizz_buzz(next_number))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    player_answer = input("Your go: ")
+    if player_answer != correct_answer:
+        print("You lose the correct answer was {}".format(correct_answer))
+        break
+else:
+    print("Well done, you reached {}".format(next_number))
