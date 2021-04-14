edf3241f061366757c28096682c8896df87e5a82
Firas Altayeb <tayebfiras@gmail.com>
2021-04-14 19:41:59 +0900

    code refactoring

--- Taisei_code/is_prime.py
+++ Taisei_code/is_prime.py
@@ -1,9 +1,9 @@
 i = 2
-while(i <= 50):
+while i <= 50:
     j = 2
-    while(j <= (i/j)):
-        if not(i%j != 0): break
+    while j <= (i / j):
+        if not (i % j != 0): break
         j = j + 1
-    if(j > (i/j)):
+    if j > (i / j):
         print(i, " is prime")
     i = i + 1
