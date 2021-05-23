a27df5de98471c4eeab3c8abf502afae4e534a08
Firas Altayeb <tayebfiras@gmail.com>
2021-05-23 20:33:02 +0900

    code refactoring

--- Minecraft/pyramid.py
+++ Minecraft/pyramid.py
@@ -3,10 +3,10 @@ from mcpi.minecraft import Minecraft
 mc = Minecraft.create("mc.tokyocodingclub.com")
 playerId = mc.getPlayerEntityId("TCCMinecraft001")
 
-x,y,z = mc.entity.getPos(playerId)
+x, y, z = mc.entity.getPos(playerId)
 
 nLayer = 20
-for l in range(1,nLayer+1):
-	for i in range(l*2-1):
-		for j in range(l*2-1):
-			mc.setBlock(x+i-l+1, y+nLayer-l, z+j-l+1, 57)
+for g in range(1, nLayer + 1):
+	for i in range(g * 2 - 1):
+		for j in range(g * 2 - 1):
+			mc.setBlock(x + i - g + 1, y + nLayer - g, z + j - g + 1, 57)
--- Minecraft/set_block.py
+++ Minecraft/set_block.py
@@ -7,6 +7,8 @@ y = position.y
 z = position.z
 
 block_id = 103
+number_of_cycles = 2 ** 4 * 3
 
-for i in range(0, 10):
+for i in range(0, number_of_cycles):
     mc.setBlock(x + i, y, z, block_id)
+
