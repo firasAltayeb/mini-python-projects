541e740f041359aad1b27a941c8fd7edd8967fcb
Firas Altayeb <tayebfiras@gmail.com>
2021-06-02 21:23:52 +0900

    fixed error with beacon creation code

--- minecraft/beacon_light.py
+++ minecraft/beacon_light.py
@@ -13,8 +13,6 @@ beaconBlock = 138
 mc.setBlocks(x, y, z, x+4, y, z+4, diamondBlock)
 # to make middle layer of beacon
 mc.setBlocks(x+1, y+1, z+1, x+3, y+1, z+3, diamondBlock)
-# to make top layer of beacon
-mc.setBlocks(x+2, y+2, z+2, x+2, y+2, z+2, diamondBlock)
 # to make beacon top
 mc.setBlock(x+2, y+3, z+2, beaconBlock)
 
