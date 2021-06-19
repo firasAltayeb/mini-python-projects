eb17dbe78560034bcaa1a9157559628a65dc32b2
Firas Altayeb <tayebfiras@gmail.com>
2021-06-19 20:19:40 +0900

    refactored giant egg code

--- minecraft/object_gen_script.py
+++ minecraft/object_gen_script.py
@@ -1,18 +1,18 @@
-file = open("input.txt", "r")
-
-outfile = open("output.py","w")
+file = open("giant_egg_input.txt", "r")
+outfile = open("giant_egg_script.py", "w")
 
 outfile.write("from mcpi.minecraft import Minecraft\n")
 outfile.write("mc = Minecraft.create()\n")
 
-outfile.write("x = 100\n")
-outfile.write("y = 30\n")
-outfile.write("z = 100\n")
-outfile.write("a = 42\n")
+outfile.write("pos_x = 100\n")
+outfile.write("pos_y = 30\n")
+outfile.write("pos_z = 100\n")
+outfile.write("block_id = 42\n")
 
-for line in file:
-    l = line[:-1].split(",")
-    outfile.write("mc.setBlock(x+"+l[0]+",y+"+l[1]+",z+"+l[2]+", a)\n")
+for lines in file:
+    line = lines[:-1].replace(" ", "").split(",")
+    outfile.write("mc.setBlock(pos_x+" + line[0] + ", pos_y+"
+                  + line[1] + ", pos_z+" + line[2] + ", block_id)\n")
 
 file.close()
 outfile.close()
