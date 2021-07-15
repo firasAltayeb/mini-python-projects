74c6437581f01b8bdd786a59afd78f444ecee091
Firas Altayeb <tayebfiras@gmail.com>
2021-07-15 21:12:08 +0900

    fixed a small bug with the commented out code

--- lesson_code/exit_game.py
+++ lesson_code/exit_game.py
@@ -6,7 +6,7 @@ available_exits = ["north"]
 exit_choice = ""
 while exit_choice not in available_exits:
     # exit_choice = input("Please choose an exit: ")
-    # random_index = random.randint(0, len(all_exits))
+    # random_index = random.randint(0, len(all_exits)-1)
     random_index = random.randrange(len(all_exits))
     exit_choice = all_exits[random_index]
     print(exit_choice)
