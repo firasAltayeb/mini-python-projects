18faaf6b383e07d720387b61ee423787afa44e9c
Firas Altayeb <tayebfiras@gmail.com>
2021-08-31 04:18:24 +0900

    improved tower of everything

--- minecraft/tower_of_everything.py
+++ minecraft/tower_of_everything.py
@@ -1,13 +1,14 @@
 import random
 from mcpi.minecraft import Minecraft
+
 mc = Minecraft.create("mc.tokyocodingclub.com")
 
 playerId = mc.getPlayerEntityId('TCCMinecraft008')
 
-x,y,z,= mc.entity.getTilePos(playerId)
+x, y, z, = mc.entity.getTilePos(playerId)
 
 all_blocks = list(range(1, 255))
 random.shuffle(all_blocks)
 for i in range(len(all_blocks)):
-    mc.setBlock(x, y+i, z+1, all_blocks[i])
-
+    if i != 8 or 9 or 10 or 11:
+        mc.setBlock(x, y + i, z + 1, all_blocks[i])
