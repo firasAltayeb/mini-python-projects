6ecdd03d8fd6b944fe84d3a237e9a66f5efe8d6e
Firas Altayeb <tayebfiras@gmail.com>
2021-11-14 19:18:49 +0900

    improved exit game code for lessons

--- lesson_code/exit_game.py
+++ lesson_code/exit_game.py
@@ -1,18 +1,18 @@
 import random
 
 all_exits = ["north", "south", "east", "west", "up", "down", "left", "right"]
-available_exits = ["north"]
 
+random_index = random.randrange(len(all_exits))
+correct_exit = all_exits[random_index]
 exit_choice = ""
-while exit_choice not in available_exits:
-    # exit_choice = input("Please choose an exit: ")
-    # random_index = random.randint(0, len(all_exits)-1)
-    random_index = random.randrange(len(all_exits))
-    exit_choice = all_exits[random_index]
-    print(exit_choice)
-    if exit_choice.casefold() in available_exits:
+
+while exit_choice.casefold() != correct_exit:
+    exit_choice = input("Please choose an exit: ")
+    # random_index = random.randrange(len(all_exits))
+    # correct_exit = all_exits[random_index]
+    if exit_choice.casefold() == correct_exit:
         print("You have escaped")
-    if exit_choice == "quit":
+    if exit_choice.casefold() == "quit":
         print("Game Over")
         break
 
