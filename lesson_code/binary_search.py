3aeacd043bee4955c2e5c64bff278d5c1b5b1cc6
Firas Altayeb <tayebfiras@gmail.com>
2022-01-10 15:58:20 +0900

    code for recursive binary search

---
+++ lesson_code/binary_search.py
@@ -0,0 +1,25 @@
+def findValue(numbers, low, high, number_to_find):
+    if low <= high:
+        middle = low + (high - low) // 2
+
+        if numbers[middle] == number_to_find:
+            return middle
+        elif numbers[middle] < number_to_find:
+            low = middle + 1
+            return findValue(numbers, low, high, number_to_find)
+        else:
+            high = middle - 1
+            return findValue(numbers, low, high, number_to_find)
+    else:
+        return -1
+
+
+numbers = [7, 9, 14, 22, 34]
+number_to_find = 22
+
+result_index = findValue(numbers, 0, len(numbers) - 1, number_to_find)
+
+if result_index == -1:
+    print("This item was not found in the list")
+else:
+    print("The number {0} was found at index position {1}".format(number_to_find, result_index))
