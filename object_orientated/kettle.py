63a0475611e7f628fc98835961bc31f4b72609ee
Firas Altayeb <tayebfiras@gmail.com>
2022-07-11 18:46:27 -0400

    intro into python classes p2

--- object_orientated/kettle.py
+++ object_orientated/kettle.py
@@ -5,6 +5,9 @@ class Kettle(object):
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 9.99)
 print(kenwood.make)
@@ -15,4 +18,16 @@ print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 15.0)
 
-print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamliton))
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+print(hamilton.on)
+hamilton.switch_on()
+
+Kettle.switch_on(kenwood)
+print(kenwood.on)
+
+print("*" * 80)
+# classes in python are dynamic
+kenwood.power = 1.5
+print(kenwood.power)
+
