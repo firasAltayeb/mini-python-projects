63552f5659ef9459e41904ee95e85f64b9f3cc6b
ObaidaHY <obaidah@mail.tau.ac.il>
2022-07-27 05:43:25 +0300

    Refining

--- lesson_code/binary_search.py
+++ lesson_code/binary_search.py
@@ -2,7 +2,7 @@
 # def print_msg(arg):
 #     print(f"The arg {arg} is printed with print msg")
 
-'''def findValue(col, low, high, num_to_find):
+'''def find_value(col, low, high, num_to_find):
     if low <= high:
         middle = low + (high - low) // 2
 
@@ -10,14 +10,14 @@
             return middle
         elif col[middle] < num_to_find:
             low = middle + 1
-            return findValue(col, low, high, num_to_find)
+            return find_value(col, low, high, num_to_find)
         else:
             high = middle - 1
-            return findValue(col, low, high, num_to_find)
+            return find_value(col, low, high, num_to_find)
     else:
         return -1'''
 
-def findValue(col,low,high,num_to_find):
+def find_value(col,low,high,num_to_find):
     while low <= high:
         middle = (low + high)//2
 
@@ -38,7 +38,7 @@ collection = [7, 9, 14, 22, 34]
 
 # print_msg(number_to_find)
 
-result_index = findValue(collection, lowest_index, len(collection) - 1, number_to_find)
+result_index = find_value(collection, lowest_index, len(collection) - 1, number_to_find)
 
 if result_index == -1:
     print("This item was not found in the list")
