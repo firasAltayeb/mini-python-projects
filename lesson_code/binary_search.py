8d9490c462311de93ecbe7a872c6b6c9cea99759
Firas Altayeb <tayebfiras@gmail.com>
2022-08-01 22:05:50 -0400

    randomized the size and value of the collection used with the binary search algo

--- lesson_code/binary_search.py
+++ lesson_code/binary_search.py
@@ -1,23 +1,13 @@
+import random
+
+
 # used to text debug tool functionality
-# def print_msg(arg):
-#     print(f"The arg {arg} is printed with print msg")
+def print_msg(num):
+    print(f"num is {num} - sprint_msg")
 
-'''def find_value(col, low, high, num_to_find):
-    if low <= high:
-        middle = low + (high - low) // 2
 
-        if col[middle] == num_to_find:
-            return middle
-        elif col[middle] < num_to_find:
-            low = middle + 1
-            return find_value(col, low, high, num_to_find)
-        else:
-            high = middle - 1
-            return find_value(col, low, high, num_to_find)
-    else:
-        return -1'''
-
-def find_value(col,low,high,num_to_find):
+def find_value(col, low, high, num_to_find):
+    # if low <= high:
     while low <= high:
         middle = (low + high)//2
 
@@ -26,17 +16,28 @@ def find_value(col,low,high,num_to_find):
 
         elif col[middle] < num_to_find:
             low = middle + 1
+            # return find_value(col, low, high, num_to_find)
 
         else:
             high = middle - 1
+            # return find_value(col, low, high, num_to_find)
     return -1
 
 
+collection = []
 lowest_index = 0
-number_to_find = 22
-collection = [7, 9, 14, 22, 34]
+# number_to_find = 27
+collection_size = random.randint(10, 100)
 
-# print_msg(number_to_find)
+for i in range(collection_size):
+    last_value = 0 if len(collection) == 0 else collection[-1]
+    rand_value = random.randint(last_value, i * 10)
+    print(f"rand_value is {rand_value}")
+    collection.append(rand_value)
+
+number_to_find = collection[random.randrange(collection_size)]
+print(f"collection length is {len(collection)}")
+print_msg(number_to_find)
 
 result_index = find_value(collection, lowest_index, len(collection) - 1, number_to_find)
 
