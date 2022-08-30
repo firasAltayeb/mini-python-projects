066753f586b572ce0de76336c1f500179a875bf3
Firas Altayeb <tayebfiras@gmail.com>
2022-08-29 23:59:30 -0500

    json parsing and serializing class p3

--- lesson_code/data_parsing.py
+++ lesson_code/data_parsing.py
@@ -1,3 +1,5 @@
+import json
+
 # gameBoard = {'1': ' ', '2': ' ', '3': ' ',
 #              '4': ' ', '5': ' ', '6': ' ',
 #              '7': ' ', '8': ' ', '9': ' '}
@@ -11,15 +13,16 @@ print(type(gameBoard))
 for i in range(9):
     gameBoard[str(i)] = ''
 
-print(gameBoard)
+# for i in range(9):
+#     with open('example.json', 'a+') as new_file:
+#         if i == 0:
+#             new_file.write('{\n')
+#             new_file.write(f'\"{i}\":\"\",\n')
+#         elif i != 0 and i != 8:
+#             new_file.write(f'\"{i}\":\"\",\n')
+#         elif i == 8:
+#             new_file.write(f'\"{i}\":\"\"\n')
+#             new_file.write('}')
 
-for i in range(9):
-    with open('example.json', 'a+') as new_file:
-        if i == 0:
-            new_file.write('{\n')
-            new_file.write(f'\"{i}\":\"\",\n')
-        if i != 0 and i != 8:
-            new_file.write(f'\"{i}\":\"\",\n')
-        if i == 8:
-            new_file.write(f'\"{i}\":\"\"\n')
-            new_file.write('}')
+with open('example.json', 'a+') as new_file:
+    json.dump(gameBoard, new_file)
