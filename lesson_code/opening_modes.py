449c447c09e838894336236cfa885c708121be78
Firas Altayeb <tayebfiras@gmail.com>
2022-09-06 22:30:59 -0500

    improved opening modes lesson code

--- lesson_code/opening_modes.py
+++ lesson_code/opening_modes.py
@@ -1,40 +1,36 @@
-import json
+message_one = " Hello world "
+message_two = " Hi there Josh "
 
-# gameBoard = {'1': ' ', '2': ' ', '3': ' ',
-#              '4': ' ', '5': ' ', '6': ' ',
-#              '7': ' ', '8': ' ', '9': ' '}
-
-gameBoard = set({})
-print(type(gameBoard))
-
-# gameBoard = {}
-# for i in range(9):
-#     gameBoard[str(i)] = ''
-
-gameBoard = {i: '' for i in range(9)}
-print(type(gameBoard))
-
-for i in range(9):
-    with open('example.json', 'a+') as new_file:
-        if i == 0:
-            new_file.write('{\n')
-            new_file.write(f'\"{i}\":\"\",\n')
-        elif i != 0 and i != 8:
-            new_file.write(f'\"{i}\":\"\",\n')
-        elif i == 8:
-            new_file.write(f'\"{i}\":\"\"\n')
-            new_file.write('}')
-
-file = open('file.txt', 'w')
-print(gameBoard, file=file)
-file.close()  # Close file
-
-# w Truncate file to zero length & open for writing (file created)
-# w+ Truncate file to zero length & open for writing & reading (file created)
-with open('example.txt', 'w') as new_file:
-    print(gameBoard, file=new_file)
+# file = open('file.txt', 'a')
+# print(message, file=file)
+# file.close()
 
 # a Open for writing at end (file created)
-# a+ Open for reading and writing at start (file created)
-with open('example.json', 'a+') as new_file:
-    json.dump(gameBoard, new_file)
+with open('file.txt', 'a') as file:
+    file.write(message_one)
+
+# a+ Open for reading & writing at end (file created)
+with open('file.txt', 'a+') as file:
+    file.write(message_two)
+    file.seek(0)
+    print(file.read())
+
+# r Open for writing at start (file not created)
+with open('file.txt', 'r') as file:
+    print(file.read())
+
+# r+ Open for reading & writing at start (file not created)
+with open('file.txt', 'r+') as file:
+    file.write(message_two * 2)
+    file.seek(0)
+    print(file.read())
+
+# w Truncate file to zero/nil & open for writing (file created)
+with open('file.txt', 'w') as file:
+    file.write(message_two)
+
+# w+ Truncate file to zero/nil & open for writing & reading (file created)
+with open('file.txt', 'w+') as file:
+    file.write(message_two * 2)
+    file.seek(0)
+    print(file.read())
