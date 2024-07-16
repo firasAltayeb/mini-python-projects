c862e20299679fa4120a829dba035069c352e903
Firas Altayeb <tayebfiras@gmail.com>
2024-07-16 14:34:39 -0500

    updated fizzbuzz to be more beginner friendly

--- lesson-code/fizzbuzz_challenge.py
+++ lesson-code/fizzbuzz_challenge.py
@@ -1,26 +1,29 @@
-def fizz_buzz(number: int) -> str:
+def fizzbuzz(number):        
     if number % 15 == 0:
-        return "fizz buzz"
-    elif number % 3 == 0:
-        return "fizz"
+        return f"fizzbuzz {number}" 
     elif number % 5 == 0:
-        return "buzz"
+        return f"fizz {number}"
+    elif number % 3 == 0:
+        return f"buzz {number}"
     else:
         return str(number)
 
-
-input("Play Fizz Buzz. Press ENTER to start")
+input(f"press enter to start: ") 
 print()
 
 next_number = 0
-while next_number < 99:
-    next_number += 1
-    print(fizz_buzz(next_number))
-    next_number += 1
-    correct_answer = fizz_buzz(next_number)
-    player_answer = input("Your go: ")
-    if player_answer != correct_answer:
-        print("You lose the correct answer was {}".format(correct_answer))
+while next_number < 30:
+    next_number = next_number + 1
+    print(f"ai turn: {fizzbuzz(next_number)}")
+
+
+    next_number = next_number + 1
+    answer = fizzbuzz(next_number)
+
+    guess = input("your turn: ")
+
+    if answer != guess:
+        print(f"you lost, the correct answer is {answer}")
         break
-else:
-    print("Well done, you reached {}".format(next_number))
+
+print(f"well done!!! :)")
